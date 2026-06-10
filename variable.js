b0fb03b45f0aa87de3113f15c15ccc30939f5326
Shubham Chaudhary <schy8202@gmail.com>
2026-06-10 16:08:56 +0530

    variable

---
+++ variable.js
@@ -0,0 +1,7 @@
+const StudentId = 12345;
+let studentName = "John Doe";
+var isEnrolled = true;
+console.log(StudentId);
+console.log(studentName);
+console.log(isEnrolled);
+console.table({StudentId, studentName, isEnrolled });
