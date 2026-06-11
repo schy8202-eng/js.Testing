80b70d12c6b737f9790aa985a792ac88e9e9e2bb
Shubham Chaudhary <schy8202@gmail.com>
2026-06-11 15:41:36 +0530

    ConversionOperation

--- ConversionOperation.js
+++ ConversionOperation.js
@@ -1,10 +1,10 @@
-const number="555tfr"
-const number=null;         //0 output
-const number=undefined;       //NaN output
-const number=true;             // 1 output
-const number="shubham";        //ye number me convert nahi ho sakta 
+//const number="555tfr"
+//const number=null;            //0 output
+//const number=undefined;       //NaN output
+//const number=true;             // 1 output
+//const number="shubham";        //ye number me convert nahi ho sakta 
 //console.log(typeof(number));
-let ConversionNumber=Number(number);
+//let ConversionNumber=Number(number);
 //console.log(ConversionNumber);
 //console.log(typeof(ConversionNumber));     //NaN means Not a Number
 
@@ -15,4 +15,25 @@ let ConversionNumber=Number(number);
 //"shubham"=>true
 let loggedIn=1;
 let isLoggedIn=Boolean(loggedIn);
-console.log(isLoggedIn);
+//console.log(isLoggedIn);
+const num=554;
+let stringNum=String(num);
+//console.log(typeof(stringNum));
+//***********************************************************operations **********************************************
+let value=8;
+let negValue=-value;
+//console.log(negValue);
+//string concatenation
+let str1="shubham";
+let str2="kumar";
+let str3=str1 + str2;
+//console.log(str3);
+/* console.log("1"+2);
+console.log((1+2)+"5");
+console.log(5+"5");
+console.log(+true);
+console.log(+""); */
+let gamecounter=500;
+gamecounter++;  //incerment by 1 (postincrement)postfix
+++gamecounter;  //incerment by 1 (preincrement)prefix
+console.log(gamecounter);
