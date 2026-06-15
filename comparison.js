9731cd4b6063769dd833f3815a090d9a8c242042
Shubham Chaudhary <schy8202@gmail.com>
2026-06-15 06:54:23 +0530

    comparison

---
+++ comparison.js
@@ -0,0 +1,11 @@
+//console.log(5>8);
+//console.log("5"<8); //automatically type conversion
+//console.log("08"<12);
+//== and comparison <,>,<=,>= work differently 
+//console.log(null>0); //false
+//console.log(null==0); //false
+//console.log(null>=0); //true reason is that comparison convert null to a number,treating it as 0.
+console.log(undefined==0); //false
+console.log("8"===8); //false yha pe type conversion nhi hota hai
+
+//Avoid to use such type codes 
