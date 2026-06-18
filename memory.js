0fd7f5cb6c86f482a4fb64b7ed0ec6928d46b8a4
Shubham Chaudhary <schy8202@gmail.com>
2026-06-18 21:12:18 +0530

    string

--- memory.js
+++ memory.js
@@ -7,7 +7,7 @@ let anotherName=name;
 anotherName="Kumar";
 //console.log(name);
 //console.log(anotherName);
-
+   
 
 //Heap Memory
 let username={
@@ -15,5 +15,25 @@ let username={
 }
 let anotheruserName=username;
 anotheruserName.email="schy3360@gmail.com"
-console.log(username);
-console.log(anotheruserName);
+//console.log(username);          //reference 
+//console.log(anotheruserName);   
+
+
+
+//string in java script
+
+const nam = new String(`shubham`);
+let year = new String(` of final year`);   //try to prefer this
+//console.log(nam);
+//let um = "kumar";
+//console.log(um);
+console.log(`hello my name is ${nam} and i am a B.tech student ${year} `);  //prefer this (it is called string interpolation)
+
+//mode of string
+
+console.log(nam[0]);
+console.log(nam[1]);
+console.log(nam.length); //length
+
+console.log(nam.toUpperCase()); //uppercase
+
